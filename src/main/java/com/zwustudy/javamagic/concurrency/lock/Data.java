7436690b80f52467d41d31fc25d4ebb0971549fb
zwustudy <zwustudy@163.com>
2018-06-26 16:45:51 +0800

    测试并发的数据结构

---
+++ src/main/java/com/zwustudy/javamagic/concurrency/lock/Data.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.zwustudy.javamagic.concurrency.lock;
+
+/**
+ * @author zwustudy
+ *
+ */
+public class Data {
+
+	private int value;
+
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+}
